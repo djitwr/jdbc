99766f5cf8ab9de02f1d990bbb100d29a77e3ccc
stack91 <501439094@qq,com>
2022-03-23 00:49:48 +0800

    分支切换到main_01

--- src/main/java/cn/stack/Test/C3p0Test.java
+++ src/main/java/cn/stack/Test/C3p0Test.java
@@ -19,7 +19,11 @@ public class C3p0Test {
 	 public void c3p0Test() throws PropertyVetoException, SQLException {
 		ComboPooledDataSource cpds = new ComboPooledDataSource();
 		cpds.setDriverClass( "com.mysql.cj.jdbc.Driver" ); //loads the jdbc driver            
+<<<<<<< HEAD
 		cpds.setJdbcUrl( "jdbc:mysql://127.0.0.1:3306/jdbc_01" );
+=======
+		cpds.setJdbcUrl( "jdbc:mysql://localhost:3306/jdbc_01" );
+>>>>>>> 147a4ac (01)
 		cpds.setUser("root");                                  
 		cpds.setPassword("123456");
 		
